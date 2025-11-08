b5e54fcbfa4a79f1678990f7fc441c672ee47b3e
noorjada <jadanoorfaris@gmail.com>
2025-11-08 03:39:22 +0200

    z

---
+++ healthpal-api/healthpal-api/src/models/Request.js
@@ -0,0 +1,33 @@
+// src/models/Request.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Request = sequelize.define('Request', {
+  id: {
+    type: DataTypes.BIGINT,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  requester_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  medication_id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM('pending', 'fulfilled', 'cancelled'),
+    defaultValue: 'pending',
+  },
+  delivery_address: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+}, {
+  tableName: 'requests',
+  timestamps: true,
+  underscored: true,
+});
+
+module.exports = Request;
